ed42c2160933c6a1078a9dda82bf32131ec24b26
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-19 17:56:17 -0400

    Avances en la implementacion de capa servicios de line

---
+++ src/main/java/cl/usach/Service/LineServiceImpl.java
@@ -0,0 +1,21 @@
+package cl.usach.Service;
+
+import cl.usach.Model.Line;
+
+public class LineServiceImpl extends LineService{
+
+    @Override
+    public int lineSectionLength(String StationName1, String StationName2) {
+        return 0;
+    }
+
+    @Override
+    public int lineSectionCost(String StationName1, String StationName2) {
+        return 0;
+    }
+
+    @Override
+    public boolean isLine(Line line) {
+        return false;
+    }
+}
