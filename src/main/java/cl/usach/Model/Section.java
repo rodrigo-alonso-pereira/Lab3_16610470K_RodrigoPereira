98e382b638de1eaa5fbf6333eecb25d63ddc1d65
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-18 23:08:51 -0400

    Implementacion de constructor Section de TDA Section

---
+++ src/main/java/cl/usach/Model/Section.java
@@ -0,0 +1,58 @@
+package cl.usach.Model;
+
+public class Section {
+
+    private Station point1;
+    private Station point2;
+    private int distance;
+    private int cost;
+
+    public Section(Station point1, Station point2, int distance, int cost) {
+        this.point1 = point1;
+        this.point2 = point2;
+        this.distance = distance;
+        this.cost = cost;
+    }
+
+    public Station getPoint1() {
+        return point1;
+    }
+
+    public void setPoint1(Station point1) {
+        this.point1 = point1;
+    }
+
+    public Station getPoint2() {
+        return point2;
+    }
+
+    public void setPoint2(Station point2) {
+        this.point2 = point2;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Section{" +
+                "point1=" + point1 +
+                ", point2=" + point2 +
+                ", distance=" + distance +
+                ", cost=" + cost +
+                '}';
+    }
+}
