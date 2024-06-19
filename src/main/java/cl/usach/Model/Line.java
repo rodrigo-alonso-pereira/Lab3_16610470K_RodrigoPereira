e337383f15ba13cb0e299e3eeab466544c560eb9
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-18 23:21:24 -0400

    Implementacion de constructor Line de TDA Line

---
+++ src/main/java/cl/usach/Model/Line.java
@@ -0,0 +1,60 @@
+package cl.usach.Model;
+
+import java.util.ArrayList;
+
+public class Line {
+
+    private int id;
+    private String name;
+    private String railType;
+    private ArrayList<Section> sections = new ArrayList<>();
+
+    public Line(int id, String name, String railType, ArrayList<Section> sections) {
+        this.id = id;
+        this.name = name;
+        this.railType = railType;
+        this.sections = sections;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRailType() {
+        return railType;
+    }
+
+    public void setRailType(String railType) {
+        this.railType = railType;
+    }
+
+    public ArrayList<Section> getSections() {
+        return sections;
+    }
+
+    public void setSections(ArrayList<Section> sections) {
+        this.sections = sections;
+    }
+
+    @Override
+    public String toString() {
+        return "Line{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", railType='" + railType + '\'' +
+                ", sections=" + sections +
+                '}';
+    }
+}
