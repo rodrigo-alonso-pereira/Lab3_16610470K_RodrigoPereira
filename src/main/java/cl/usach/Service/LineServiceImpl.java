0d0d9529a351c05a6efe56e57ac46ea8f9a1f6f5
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-21 00:04:59 -0400

    Avances en isLine

--- src/main/java/cl/usach/Service/LineServiceImpl.java
+++ src/main/java/cl/usach/Service/LineServiceImpl.java
@@ -2,20 +2,18 @@ package cl.usach.Service;
 
 import cl.usach.Model.Line;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class LineServiceImpl implements LineService {
 
     @Override
-    public int lineSectionLength(String StationName1, String StationName2) {
-        return 0;
-    }
-
-    @Override
-    public int lineSectionCost(String StationName1, String StationName2) {
-        return 0;
-    }
-
-    @Override
     public boolean isLine(Line line) {
+        List<Integer> idList = line.getSections().stream()
+                .flatMap(Collection::stream)
+                .map(ob->(Integer)ob)
+                .collect(Collectors.toList());
         return false;
     }
 }
