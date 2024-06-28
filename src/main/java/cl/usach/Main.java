452586323920a32c64518db94e37dd403302d3dd
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-27 22:39:11 -0400

    Agregan datos de prueba station

--- src/main/java/cl/usach/Main.java
+++ src/main/java/cl/usach/Main.java
@@ -1,7 +1,21 @@
 package cl.usach;
 
+import cl.usach.Model.Station;
+
+import static cl.usach.Model.StationType.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Station st0 = new Station(0, "San Alberto Hurtado", TERMINAL, 35);
+        Station st1 = new Station(1, "USACH", COMBINACION, 30);
+        Station st2 = new Station(2, "Estación Central", COMBINACION, 35);
+        Station st3 = new Station(3, "ULA", REGULAR, 35);
+        Station st4 = new Station(4, "República", REGULAR, 35);
+        Station st5 = new Station(5, "Los Héroes", COMBINACION, 35);
+        Station st6 = new Station(6, "La Moneda", REGULAR, 35);
+        Station st7 = new Station(7, "U. de Chile", COMBINACION, 35);
+        Station st8 = new Station(8, "Santa Lucia", TERMINAL, 35);
+        System.out.println(st8.toString());
     }
 }
