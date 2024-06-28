e48430d7103121d84fac6bebaac5e6302f70e3db
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-06-27 22:55:21 -0400

    Se agregan datos de prueba para section

--- src/main/java/cl/usach/Main.java
+++ src/main/java/cl/usach/Main.java
@@ -1,5 +1,6 @@
 package cl.usach;
 
+import cl.usach.Model.Section;
 import cl.usach.Model.Station;
 
 import static cl.usach.Model.StationType.*;
@@ -7,6 +8,7 @@ import static cl.usach.Model.StationType.*;
 public class Main {
     public static void main(String[] args) {
 
+        //Datos station
         Station st0 = new Station(0, "San Alberto Hurtado", TERMINAL, 35);
         Station st1 = new Station(1, "USACH", COMBINACION, 30);
         Station st2 = new Station(2, "Estación Central", COMBINACION, 35);
@@ -16,6 +18,18 @@ public class Main {
         Station st6 = new Station(6, "La Moneda", REGULAR, 35);
         Station st7 = new Station(7, "U. de Chile", COMBINACION, 35);
         Station st8 = new Station(8, "Santa Lucia", TERMINAL, 35);
-        System.out.println(st8.toString());
+
+        //Datos de section
+        Section s0 = new Section(st0, st1, 2, 50);
+        Section s1 = new Section(st1, st2, 2.5, 55);
+        Section s2 = new Section(st2, st3, 1.5, 30);
+        Section s3 = new Section(st3, st4, 3, 45);
+        Section s4 = new Section(st4, st5, 3, 45);
+        Section s5 = new Section(st5, st6, 1.4, 50);
+        Section s6 = new Section(st6, st7, 2, 40);
+        Section s7 = new Section(st7, st8, 3, 20);
+
+
+        System.out.println(s7.toString());
     }
 }
