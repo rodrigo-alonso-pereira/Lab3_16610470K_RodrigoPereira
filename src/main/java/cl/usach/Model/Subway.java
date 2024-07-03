7512814e72d90eda3ab84e869e6da26824a0375f
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-07-03 17:03:32 -0400

    Implementacion de constructor Subway de TDA subway

---
+++ src/main/java/cl/usach/Model/Subway.java
@@ -0,0 +1,72 @@
+package cl.usach.Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Subway{
+
+    private int id;
+    private String name;
+    private List<Line> lines;
+    private List<Train> trains;
+    private List<Driver> drivers;
+
+    public Subway(int id, String name) {
+        this.id = id;
+        this.name = name;
+        this.lines = new ArrayList<>();
+        this.trains = new ArrayList<>();
+        this.drivers = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<Line> lines) {
+        this.lines = lines;
+    }
+
+    public List<Train> getTrains() {
+        return trains;
+    }
+
+    public void setTrains(List<Train> trains) {
+        this.trains = trains;
+    }
+
+    public List<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(List<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    @Override
+    public String toString() {
+        return "Subway{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lines=" + lines +
+                ", trains=" + trains +
+                ", drivers=" + drivers +
+                '}';
+    }
+}
