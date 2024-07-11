246aa6baca596f09bdfaff2d40ac4e2fa4208c5b
Rodrigo Pereira <rodrigo.pereira@usach.cl>
2024-07-11 06:27:26 -0400

    Ajustes finales

--- src/main/java/cl/usach/Service/TrainServiceImpl.java
+++ src/main/java/cl/usach/Service/TrainServiceImpl.java
@@ -8,7 +8,7 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class TrainServiceImpl implements TrainService {
-    Utililty util = new Utililty();
+    private Utililty util = new Utililty();
 
     /**
      *
